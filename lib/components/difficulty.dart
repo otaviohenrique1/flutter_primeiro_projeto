adddb67bd4c46fff18084550105eee1b3e5f30a0
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-01-26 21:51:02 -0300

    commit 16

--- lib/components/difficulty.dart
+++ lib/components/difficulty.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_primeiro_projeto/components/star.dart';
 
 class Difficulty extends StatelessWidget {
   final int dificultyLevel;
@@ -20,30 +21,6 @@ class Difficulty extends StatelessWidget {
         Star(dificultyLevel: dificultyLevel, starNumber: 4),
         Star(dificultyLevel: dificultyLevel, starNumber: 5),
       ],
-    );
-  }
-}
-
-class Star extends StatelessWidget {
-  final int dificultyLevel;
-  final int starNumber;
-
-  const Star({
-    Key? key,
-    required this.dificultyLevel,
-    required this.starNumber,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var blue = Colors.blue;
-    var lightBlue = Colors.blue[100];
-    var validateDifficuty = dificultyLevel >= starNumber;
-
-    return Icon(
-      Icons.star,
-      size: 15,
-      color: (validateDifficuty) ? blue : lightBlue,
     );
   }
 }
